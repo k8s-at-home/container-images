2a24f7ccbaa52b6a0ba053a6d227fee94d6da4a3
bjws <bernd@bjws.nl>
2020-11-11 13:50:49 +0100

    Move killswitch to separate shim

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-KILLSWITCH=${KILLSWITCH:-"false"}
-KILLSWITCH_EXCLUDEDNETWORKS=${KILLSWITCH_EXCLUDEDNETWORKS:-""}
-
 if [[ "$(cat /proc/sys/net/ipv4/conf/all/src_valid_mark)" != "1" ]]; then
     echo "[WARNING] sysctl net.ipv4.conf.all.src_valid_mark=1 is not set" >&2
 fi
@@ -20,28 +17,7 @@ INTERFACE="${CONFIG%.*}"
 
 sudo /usr/bin/wg-quick up "${INTERFACE}"
 
-if
-    [[ "${KILLSWITCH}" == "true" ]];
-then
-    DEFAULTROUTE_IPV4=$(/usr/sbin/ip -4 route | grep default | awk '{print $3}')
-    sudo /usr/sbin/iptables -F OUTPUT
-    sudo /usr/sbin/ip6tables -F OUTPUT
-
-    if 
-        [[ -n "$KILLSWITCH_EXCLUDEDNETWORKS" ]];
-    then
-        IFS=';' read -r -a networks <<< "${KILLSWITCH_EXCLUDEDNETWORKS}"
-        for entry in "${networks[@]}"
-        do
-            echo "[INFO] Excluding ${entry} from VPN traffic"
-            sudo /usr/sbin/ip -4 route add "${entry}" via "$DEFAULTROUTE_IPV4"
-            sudo /usr/sbin/iptables -A OUTPUT -d "${entry}" -j ACCEPT
-        done
-
-        sudo /usr/sbin/iptables -A OUTPUT ! -o "$INTERFACE" -m mark ! --mark $(sudo /usr/bin/wg show "$INTERFACE" fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
-        sudo /usr/sbin/ip6tables -A OUTPUT ! -o "$INTERFACE" -m mark ! --mark $(sudo /usr/bin/wg show "$INTERFACE" fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
-    fi
-fi
+source "/shim/killswitch.sh"
 
 _shutdown () {
     echo "[INFO] Caught signal, shutting down VPN!"
