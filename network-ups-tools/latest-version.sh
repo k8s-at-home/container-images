01b15ce399b4da83572ef81f2d453f53e427625a
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <6213398+bjw-s@users.noreply.github.com>
2020-12-23 09:41:22 +0100

    Update version logic

--- network-ups-tools/latest-version.sh
+++ network-ups-tools/latest-version.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-printf "latest"
+git clone -q -b master https://github.com/networkupstools/nut.git "/tmp/nut"
+version=$(cd /tmp/nut && git describe master)
+version="${version#*v}"
+version="${version#*release-}"
+rm -rf /tmp/nut
+printf "%s" "${version}"
