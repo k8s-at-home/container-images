41d191c1f1baac05ee94a426b722604f144de1ac
Chris Deadlock <cdeadlock@spinemed.com>
2021-02-04 13:40:42 -0800

    Add upsmon task support
    Signed-off-by: Chris Deadlock <cdeadlock@spinemed.com>

--- network-ups-tools/entrypoint.sh
+++ network-ups-tools/entrypoint.sh
@@ -2,4 +2,8 @@
 set -e
 
 /usr/local/ups/sbin/upsdrvctl -u root start
+if test -f "/etc/nut/upsmon.conf"; then
+    #upsmon configuration exists, so run it
+    (sleep 10; /usr/local/ups/sbin/upsmon -u root)&
+fi
 exec /usr/local/ups/sbin/upsd -u root -D
