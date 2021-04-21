9be56616253f4832e349a02ad3dbf31d169483f7
Nicholas Wilde <ncwilde43@gmail.com>
2021-04-20 18:52:20 -0700

    - Changed source to upstream, seejohnrun. - Added greeting.sh to entrypoint.sh - Added all ENV variables.

--- apps/haste/entrypoint.sh
+++ apps/haste/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 #shellcheck disable=SC1091
+source "/shim/greeting.sh"
 source "/shim/umask.sh"
 source "/shim/vpn.sh"
+
+set -e
+
+node ./docker-entrypoint.js > ./config.js
 
 exec node /app/server.js ${@} ${EXTRA_ARGS}
