812d44e4adc7353ab51c29fed440ca8c0e2b65ef
Jason Whitelock <jase@oshelp.co.uk>
2021-05-19 23:14:41 +0100

    tweaks as kah

--- apps/leaf2mqtt/latest-version.sh
+++ apps/leaf2mqtt/latest-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git clone -q -b master https://github.com/Troon/leaf2mqtt.git "/tmp/leaf2mqtt"
-version=$(cd /tmp/leaf2mqtt && git describe master)
+version=$(cd /tmp/leaf2mqtt && git rev-parse --short HEAD)
 version="${version#*v}"
 version="${version#*release-}"
 rm -rf /tmp/leaf2mqtt
