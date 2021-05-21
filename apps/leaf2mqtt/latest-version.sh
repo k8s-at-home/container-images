6cfeecb1e9e7f6a5b3ff7d86418187c84d6a1498
Jason Whitelock <jase@oshelp.co.uk>
2021-05-21 21:14:13 +0100

    latest version to GH API

--- apps/leaf2mqtt/latest-version.sh
+++ apps/leaf2mqtt/latest-version.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-git clone -q -b main https://github.com/mitsumaui/leaf2mqtt.git "/tmp/leaf2mqtt"
-version=$(cd /tmp/leaf2mqtt && git describe main --tags)
+version=$(curl -sX GET "https://api.github.com/repos/mitsumaui/leaf2mqtt/releases/latest" | jq --raw-output '.tag_name')
 version="${version#*v}"
 version="${version#*release-}"
-rm -rf /tmp/leaf2mqtt
 printf "%s" "${version}"
